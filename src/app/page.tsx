a09a996288ac48171b3c90e34c4caf27a4840edb
Nathan Dean <nathandean@hotmail.co.uk>
2025-06-05 00:45:03 +0100

    Add HeroSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { client } from "@/sanity/lib/client";
+import HeroSection from "@/components/HeroSection";
 import ProjectCard from "@/components/ProjectCard";
 import Project from "@/types/Project";
 
@@ -13,9 +14,11 @@ export default async function Home() {
   
     <>
 
-        {projects.map(project => (
-          <ProjectCard key = {project.slug} project = {project} />
-        ))}
+      <HeroSection />
+
+      {projects.map(project => (
+        <ProjectCard key = {project.slug} project = {project} />
+      ))}
   
     </>
   
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,13 @@
+export default function HeroSection(){
+
+    return (
+
+        <section className = "flex justify-center items-center h-screen">
+
+            <h1>Nathan Dean</h1>
+
+        </section>
+
+    )
+
+}
