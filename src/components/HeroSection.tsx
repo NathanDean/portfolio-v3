4d3e3fb7947e42a53abdbede0738eca3fd5e9e3b
Nathan Dean <nathandean@hotmail.co.uk>
2025-06-05 01:27:55 +0100

    Add basic styling and content to HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,30 @@
+import { Github, Linkedin } from 'lucide-react'
+import Link from 'next/link'
+
 export default function HeroSection(){
 
     return (
 
-        <section className = "flex justify-center items-center h-screen">
+        <section className = "flex flex-col justify-center items-center h-screen bg-[url('/water.jpg')] bg-cover bg-center bg-no-repeat">
 
-            <h1>Nathan Dean</h1>
+            <div className = "w-3/4 md:w-2/5 space-y-4">
+            
+                <h1>Hi, I'm Nathan</h1>
+
+                <h2>An MSc Computer Science & Artificial Intelligence student with a background in communications and the creative industries.</h2>
+
+                <div className = "flex space-x-4">
+
+                    <Link href = "https://github.com/nathandean">
+                        <Github size = {35} className = "hover:text-white transition duration-100"/>
+                    </Link>
+                    <Link href = "https://www.linkedin.com/in/nathanjdean/">
+                        <Linkedin size = {35} className = "hover:text-white transition duration-100" />
+                    </Link>
+
+                </div>
+
+            </div>
 
         </section>
 
