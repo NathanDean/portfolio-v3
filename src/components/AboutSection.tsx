d14d746e30737d0bb927cbe0e3e8d79f2bf86ac7
Nathan Dean <nathandean@hotmail.co.uk>
2025-06-06 01:24:36 +0100

    Change AboutSection to import bio text from Sanity and display as portable text

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,18 +1,22 @@
-export default function AboutSection(){
+import { PortableText } from '@portabletext/react'
+import { PortableTextBlock } from 'next-sanity';
+import { client } from '@/sanity/lib/client';
+import createPortableTextComponents from "@/utils/portableTextComponents"
+
+export default async function AboutSection(){
+
+    const BIO_QUERY = `*[_type == "bio"][0].bio`;
+    const options = { next: { revalidate: 30 } };
+    const bio = await client.fetch<PortableTextBlock[]>(BIO_QUERY, {}, options);
+    const components = createPortableTextComponents();
 
     return(
 
-        <div className = "flex justify-center items-center min-h-screen bg-blue-200">
+        <div className = "flex justify-center items-center min-h-screen p-10 bg-blue-200">
 
-            <div className = "w-5/8 space-y-2">
+            <div className = "w-5/8 space-y-2 text-xl">
 
-            <p>I'm baby pour-over marxism jawn +1 DSA copper mug gastropub tbh 8-bit raw denim. Slow-carb hell of irony fixie. Intelligentsia food truck readymade, cred shoreditch venmo echo park. Pour-over glossier taiyaki banjo blackbird spyplane paleo selvage shoreditch activated charcoal poke selfies XOXO. Poke cupping heirloom venmo literally, letterpress stumptown bitters. Butcher DSA brunch, typewriter subway tile readymade microdosing live-edge portland DIY messenger bag actually big mood bruh iceland. YOLO small batch tonx hammock williamsburg, church-key irony praxis pitchfork before they sold out waistcoat man braid bruh taxidermy disrupt.</p>
-
-            <p>Copper mug readymade humblebrag kogi photo booth ennui thundercats pour-over bespoke flexitarian food truck next level gentrify actually. Vibecession food truck you probably haven't heard of them, la croix letterpress pork belly fit salvia post-ironic vexillologist flannel messenger bag kale chips. Occupy austin tousled sriracha palo santo organic four loko godard selvage lo-fi tumblr disrupt poke swag. Shabby chic neutral milk hotel street art cray. Celiac knausgaard taiyaki vibecession viral live-edge schlitz tumeric portland cred venmo normcore pok pok hell of.</p>
-
-            <p>Kogi pour-over artisan schlitz tumblr, gentrify gatekeep direct trade bodega boys poutine celiac sriracha typewriter 90's la croix. Freegan DSA YOLO single-origin coffee, tonx kogi paleo 8-bit blog narwhal. Before they sold out copper mug gochujang taxidermy tbh biodiesel, butcher scenester green juice forage shoreditch. Austin health goth asymmetrical, palo santo adaptogen master cleanse cliche keytar swag whatever viral hot chicken. Fashion axe cardigan pinterest, forage sriracha PBR&B cliche jianbing asymmetrical. Sartorial praxis +1, migas retro letterpress tousled.</p>
-
-            <p>Dummy text? More like dummy thicc text, amirite?</p>
+                <PortableText value = {bio} components = {components} />
             
             </div>
         </div>
