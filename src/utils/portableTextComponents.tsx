64f83320834918835b798fef9d3554f93283c20d
Nathan Dean <nathandean@hotmail.co.uk>
2025-06-09 03:05:35 +0100

    Add text-inherit class to portable text components

--- src/utils/portableTextComponents.tsx
+++ src/utils/portableTextComponents.tsx
@@ -3,7 +3,7 @@ import { PortableTextComponents } from 'next-sanity';
 export default function createPortableTextComponents() {
   const components: PortableTextComponents = {
     block: {
-      normal: ({ children }) => <p className="pt-normal">{children}</p>,
+      normal: ({ children }) => <p className="text-inherit">{children}</p>,
     },
   };
 
