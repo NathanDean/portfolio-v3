636026a544bd329564702d9c755940d508fc9810
Nathan Dean <nathandean@hotmail.co.uk>
2025-06-13 01:34:22 +0100

    Remove Header import from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import AboutSection from '@/components/AboutSection';
-import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import ProjectSection from '@/components/ProjectSection';
 
