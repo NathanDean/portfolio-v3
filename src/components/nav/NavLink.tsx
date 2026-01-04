79e835711b9515bac23d0d37f60169263928dad3
Nathan Dean <nathandean@hotmail.co.uk>
2026-01-04 22:04:15 +0000

    Remove JS smooth scrolling function which is redundant given CSS smooth scrolling behaviour

--- src/components/nav/NavLink.tsx
+++ src/components/nav/NavLink.tsx
@@ -13,30 +13,12 @@ export default function NavLink({
   text,
   setIsMenuOpen,
 }: NavLinkProps) {
-  // Scrolling behaviour
-  const scrollToSection = (sectionId: string) => {
-    const prefersReducedMotion = window.matchMedia(
-      '(prefers-reduced-motion: reduce)'
-    ).matches;
-    const element = document.getElementById(sectionId);
-    if (element) {
-      element.scrollIntoView({
-        behavior: prefersReducedMotion ? 'auto' : 'smooth',
-        block: 'start',
-      });
-    }
-  };
-
   return (
     <a
       key={title}
       className={`hover:text-white hover:drop-shadow-[0_2px_10px_rgba(0,0,0,0.7)] ${className}`}
       href={`#${title}`}
-      onClick={(e) => {
-        if (typeof window !== 'undefined' && window.location.pathname === '/') {
-          e.preventDefault();
-        }
-        scrollToSection(title);
+      onClick={() => {
         if (setIsMenuOpen) {
           setIsMenuOpen(false);
         }
